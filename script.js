f0353f7287093432adb692fbc98f031b90c8d8c9
Superdash <pablorsantana@hotmail.com>
2022-02-24 22:23:16 -0300

    protótipo de função 'remover checkbox selecionados' adicionada

--- script.js
+++ script.js
@@ -11,3 +11,60 @@ document.getElementById("botao").addEventListener("click",function(){
     lista.appendChild(checkbox);
     lista.appendChild(label);
 })
+
+document.getElementById("remover").addEventListener("click",function(){
+
+    let checkboxes = document.querySelectorAll("input[type='checkbox']");
+    let labels = document.getElementsByTagName("label");
+    for (let index = 0; index < checkboxes.length; index++) {
+        if(checkboxes[index].checked){
+            checkboxes[index].remove()
+            labels[index].remove();
+        }
+    }
+    test()
+})
+
+function test(){
+    let checkboxes = document.querySelectorAll("input[type='checkbox']");
+    console.log(checkboxes.length)
+
+    
+    var lista = document.getElementById("lista");
+
+   
+    //console.log(lista.children)
+    //lista.remove(lista.children);
+    //lista.innerHTML = ""; 
+    while (lista.firstChild) { //Enquanto a lista tiver filhos
+        lista.removeChild(lista.firstChild); //os filhos são removidos
+    }
+    //console.log(lista)
+    for (let index = 0; index < checkboxes.length; index++) {     
+        
+            let tarefa = document.getElementById("tarefa");
+
+            var label = document.createElement("label");
+            label.appendChild(document.createTextNode(tarefa.value))
+
+            var checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+
+            lista.appendChild(label);
+            lista.appendChild(checkbox);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
