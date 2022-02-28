8833168a8df7fe1bc9b3a8462de992e13ad63784
Superdash <pablorsantana@hotmail.com>
2022-02-28 16:54:05 -0300

    remoção da função 'reconstruirLista' (obsoleta após último commit)

--- script.js
+++ script.js
@@ -56,63 +56,14 @@ document.getElementById("remover").addEventListener("click",function(){
         if(checkboxes[index].checked){
             removeValFromIndex.push(index);
         }
-        
     }
     
     for (let i of removeValFromIndex.reverse()) {
-        console.log(arrayTarefas)
         arrayTarefas.splice(i,1);
-
-        console.log(arrayTarefas)
         checkboxes[i].remove()
         labels[i].remove()
     }
     
     localStorage.setItem("Tarefas",JSON.stringify(arrayTarefas));
-
-    /*
-    checkboxes.forEach((element,index) => {
-        if(element.checked){
-            //arrayTarefas;
-            //element.remove()
-            //labels[index].remove();
-
-
-        }
-    });*/
-
-  //  reconstruirLista();
 })
 
-function reconstruirLista(){
-    let checkboxes = document.querySelectorAll("input[type='checkbox']");
-    let labels = document.querySelectorAll("label");
-    var lista = document.getElementById("lista");
-
-    while (lista.firstChild) { //Enquanto a lista tiver filhos
-        lista.removeChild(lista.firstChild); //os filhos são removidos
-    }
-
-    for (let index = 0; index < checkboxes.length; index++) {     
-        
-            var label = document.createElement("label");
-            label.appendChild(document.createTextNode(labels[index].textContent))
-
-            var checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-
-            lista.appendChild(checkbox);
-            lista.appendChild(label);
-    }
-}
-
-/*
-const valuesArr = ["v1", "v2", "v3", "v4", "v5"],
-  removeValFromIndex = [0, 2, 4];
-
-for (const i of removeValFromIndex.reverse()) {
-  valuesArr.splice(i, 1);
-}
-console.log(valuesArr)
-
-*/
