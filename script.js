1ce54cd7d695de25ff29f53eb8f9631016876aae
Superdash <pablorsantana@hotmail.com>
2022-02-28 16:52:53 -0300

    Refatoração e correção da rotina de remoção de checkboxes e labels

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+//localStorage.removeItem("Tarefas");
+let arrayTarefas = localStorage.getItem("Tarefas") ? JSON.parse(localStorage.getItem("Tarefas")) : [];
+
+if(localStorage.getItem("Tarefas")){
+    carregarTarefas()
+}
+function carregarTarefas(){
+    var lista = document.getElementById("lista");
+    arrayTarefas.forEach(element =>{
+        
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    var label = document.createElement("label");
+    label.appendChild(document.createTextNode(element))
+
+        lista.appendChild(checkbox);
+        lista.appendChild(label);
+
+    });/*
+    arrayTarefas.forEach(element => {
+        lista.innerHTML+= `<input type="checkbox"> ${element} <br><br>`
+    });*/
+}
 
 document.getElementById("botao").addEventListener("click",function(){
     var lista = document.getElementById("lista");
@@ -10,6 +34,15 @@ document.getElementById("botao").addEventListener("click",function(){
 
     lista.appendChild(checkbox);
     lista.appendChild(label);
+
+    arrayTarefas.push(tarefa.value);
+    console.log(arrayTarefas);
+
+    tarefa.value = "";
+    tarefa.focus();
+
+    localStorage.setItem("Tarefas",JSON.stringify(arrayTarefas));
+
 })
 
 document.getElementById("remover").addEventListener("click",function(){
@@ -17,22 +50,43 @@ document.getElementById("remover").addEventListener("click",function(){
     let checkboxes = document.querySelectorAll("input[type='checkbox']");
     let labels = document.querySelectorAll("label");
     
+    var removeValFromIndex = [];
+
+    for (let index = 0; index < checkboxes.length; index++) {
+        if(checkboxes[index].checked){
+            removeValFromIndex.push(index);
+        }
+        
+    }
+    
+    for (let i of removeValFromIndex.reverse()) {
+        console.log(arrayTarefas)
+        arrayTarefas.splice(i,1);
+
+        console.log(arrayTarefas)
+        checkboxes[i].remove()
+        labels[i].remove()
+    }
+    
+    localStorage.setItem("Tarefas",JSON.stringify(arrayTarefas));
+
+    /*
     checkboxes.forEach((element,index) => {
         if(element.checked){
-            element.remove()
-            labels[index].remove();
+            //arrayTarefas;
+            //element.remove()
+            //labels[index].remove();
+
+
         }
-    });
-    reconstruirLista()
+    });*/
+
+  //  reconstruirLista();
 })
 
 function reconstruirLista(){
     let checkboxes = document.querySelectorAll("input[type='checkbox']");
-
     let labels = document.querySelectorAll("label");
-
-    let tarefa = document.getElementById("tarefa");
-
     var lista = document.getElementById("lista");
 
     while (lista.firstChild) { //Enquanto a lista tiver filhos
@@ -52,15 +106,13 @@ function reconstruirLista(){
     }
 }
 
+/*
+const valuesArr = ["v1", "v2", "v3", "v4", "v5"],
+  removeValFromIndex = [0, 2, 4];
 
+for (const i of removeValFromIndex.reverse()) {
+  valuesArr.splice(i, 1);
+}
+console.log(valuesArr)
 
-
-
-
-
-
-
-
-
-
-
+*/
